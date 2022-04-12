bc9c4c4442496b1d0075fd740c73efb7f0ef459f
Caslab <Caslab>
2022-04-12 10:47:51 +0200

    modificacion de usuarios telemetria sin acabar

--- PrimeraAplicacion/Base_de_datos/seleccionar_tablas.sql
+++ PrimeraAplicacion/Base_de_datos/seleccionar_tablas.sql
@@ -17,3 +17,16 @@
 	"ute_ruta":"",
 	"ute_empresa":"",
 	"ust_token":"7887186b33749971de515859532def15f4b210eb"}')*/
+	
+SELECT * FROM public.crear_usuarios_telemetria('{
+	"ute_emp_cod":"60",
+	"ute_nombre":"pruebausuariostelemetria",
+	"ute_pwd":"",
+	"ute_auto_pwd":"true",
+	"ute_centro_padre":"",
+	"ute_centro":"2",
+	"ute_pdv":"",
+	"ute_jefe_area":"",
+	"ute_ruta":"",
+	"ute_empresa":"0",
+	"ust_token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Ijc4ODcxODZiMzM3NDk5NzFkZTUxNTg1OTUzMmRlZjE1ZjRiMjEwZWIiLCJpYXQiOjE2NDkzNDUyMzV9.olI-c3Zzl-QsCIgSDmhJ5QY71O7eL2d1mhDOrQSkP2k"}')
