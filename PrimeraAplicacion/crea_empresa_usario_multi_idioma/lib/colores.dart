b9f029446e9928a1fb98fa5242a05b107c45ffd2
Caslab <Caslab>
2022-05-11 10:00:15 +0200

    Añadido Colores Para la personalizacion

---
+++ PrimeraAplicacion/crea_empresa_usario_multi_idioma/lib/colores.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class PaletaColores {
+  // Color azul oscuro
+  static const MaterialColor colorAzulOscuro =
+      MaterialColor(0xff5938FF, <int, Color>{});
+
+  //  Color verde claro
+  static const MaterialColor colorVerde =
+      MaterialColor(0xFF37EDB6, <int, Color>{});
+
+  //  Color rojo
+  static const MaterialColor colorRojo =
+      MaterialColor(0xFFFF5C68, <int, Color>{});
+
+  //  Color gris oscuro
+  static const MaterialColor colorGrisOscuro =
+      MaterialColor(0xFF575757, <int, Color>{});
+
+  // Color gris claro
+  static const MaterialColor colorGrisClaro =
+      MaterialColor(0xFFC6C6C6, <int, Color>{});
+
+  static const List<Color> listaColores = [colorAzulOscuro, colorVerde];
+}
+
+PreferredSizeWidget appBarColoreada(String titulo) {
+  return AppBar(
+    title: Text(titulo, style: const TextStyle()),
+    flexibleSpace: Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: PaletaColores.listaColores,
+        ),
+      ),
+      child: const Align(alignment: Alignment.center),
+    ),
+  );
+}
+
+StatelessWidget drawHeaderColoreado(String titulo) {
+  return DrawerHeader(
+    child: Text(titulo),
+    decoration: const BoxDecoration(
+      gradient: LinearGradient(
+          colors: PaletaColores.listaColores, tileMode: TileMode.clamp),
+    ),
+  );
+}
+
+// Uso para colores normales??
+// Map<int, Color> colorVerde = {
+//   900: const Color.fromRGBO(55, 237, 182, 1),
+// };
+//    MaterialColor azulOscuro = MaterialColor(0xFF5938FF, colorAzul);
+//    MaterialColor verdeClaro = MaterialColor(0xFF37EDB6, colorVerde);
